e9d69b59fde9da30324146b4892a3af529b41bca
verissimohenry <verissimohenry04@gmail.com>
2021-05-11 10:11:03 +0100

    fix linter error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: %i[index]
-  def index;
-    @posts = Post.all 
+  def index
+    @posts = Post.all
   end
 end
